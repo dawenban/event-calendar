a5ac411ddd5e076e81136eac3921a8791c623f2c
dawenban <darrenvan.damatan@gmail.com>
2022-01-14 10:09:10 +0800

    minor fix layout

--- resources/views/add_event.blade.php
+++ resources/views/add_event.blade.php
@@ -9,64 +9,64 @@ Eventor
 			<div class="col-4">
 				<div class="row">
 					<div class="row">
-						<div class="form-group mb-3">
+						<div class="mb-3">
 							<label class="form-label">Event</label>
 							<input type="text" name="event_name" class="form-control" form="event_form">
 						</div>
 					</div>
 					<div class="row mb-3">
 						<div class="col-6">
-							<div class="form-group">
+							<div class="">
 								<label class="form-label">From</label>
 								<input type="date" name="date_from" class="form-control form-control-sm" value="" form="event_form">
 							</div>
 						</div>
 						<div class="col-6">
-							<div class="form-group">
+							<div class="">
 								<label class="form-label">To</label>
 								<input type="date" name="date_to" class="form-control form-control-sm" value="" form="event_form">
 							</div>
 						</div>
 					</div>
 					<div class="row mb-3">{{-- weekdays --}}
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="1" class="form-check-input" id="w_1">
 								<label for="w_1" class="form-check-label">Mon</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="2" class="form-check-input" id="w_2">
 								<label for="w_2" class="form-check-label">Tue</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="3" class="form-check-input" id="w_3">
 								<label for="w_3" class="form-check-label">Wed</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="4" class="form-check-input" id="w_4">
 								<label for="w_4" class="form-check-label">Thu</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="5" class="form-check-input" id="w_5">
 								<label for="w_5" class="form-check-label">Fri</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="6" class="form-check-input" id="w_6">
 								<label for="w_6" class="form-check-label">Sat</label>
 							</div>
 						</div>
-						<div class="col w_day">
-							<div class="form-check">
+						<div class="col p-0 w_day">
+							<div class="form-check form-control form-control-sm">
 								<input type="checkbox" name="weekday[]" form="event_form" value="0" class="form-check-input" id="w_7">
 								<label for="w_7" class="form-check-label">Sun</label>
 							</div>
